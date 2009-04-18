2fbef41e3fcea14dcfc1bdaa0e6e103e246c3f90
yusuke <yusuke@117b7e0d-5933-0410-9d29-ab41bb01d86b>
2009-04-18 16:15:13 +0000

    TFJ-114 fixing test unit that was broken with the last commit
    git-svn-id: http://yusuke.homeip.net/svn/twitter4j/trunk@234 117b7e0d-5933-0410-9d29-ab41bb01d86b

--- src/test/java/twitter4j/TwitterTestUnit.java
+++ src/test/java/twitter4j/TwitterTestUnit.java
@@ -209,7 +209,7 @@ public class TwitterTestUnit extends TestCase {
     public void testGetAuthenticatedUser() throws Exception {
         assertEquals(id1, twitterAPI1.getAuthenticatedUser().getScreenName());
         assertEquals(id1, new Twitter(id3, pass3).getAuthenticatedUser().getName());
-        assertEquals(id3,new Twitter(id3, pass3).verifyCredentials().getName());
+        assertEquals(id1,new Twitter(id3, pass3).verifyCredentials().getName());
     }
 
 
