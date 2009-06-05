d82e73ab97378410cf9274c72b03b77c6ad3b1ff
yusuke <yusuke@117b7e0d-5933-0410-9d29-ab41bb01d86b>
2009-06-05 06:50:32 +0000

    TFJ-166 GetTimelines.cmd fails with java.lang.NoClassDefFoundError: twitter4j/examples/AsyncExample
    git-svn-id: http://yusuke.homeip.net/svn/twitter4j/trunk@324 117b7e0d-5933-0410-9d29-ab41bb01d86b

--- bin/getTimelines.cmd
+++ bin/getTimelines.cmd
@@ -2,7 +2,7 @@ echo off
 SETLOCAL enabledelayedexpansion
 call setEnv.cmd
 
-echo %JAVA_HOME%\bin\java %MEM_ARGS% -classpath "%CLASSPATH%" twitter4j.examples.AsyncExample %*
-"%JAVA_HOME%\bin\java" %MEM_ARGS% -classpath "%CLASSPATH%" twitter4j.examples.AsyncExample %*
+echo %JAVA_HOME%\bin\java %MEM_ARGS% -classpath "%CLASSPATH%" twitter4j.examples.GetTimelines %*
+"%JAVA_HOME%\bin\java" %MEM_ARGS% -classpath "%CLASSPATH%" twitter4j.examples.GetTimelines %*
 
 ENDLOCAL
