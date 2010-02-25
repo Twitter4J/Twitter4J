3c82b1c3aa8f20047157524f013f4e441f82ac31
yusuke <yusuke@117b7e0d-5933-0410-9d29-ab41bb01d86b>
2010-02-25 16:45:36 +0000

    TFJ-307 add bin/oauthUpdate.sh/cmd
    git-svn-id: http://yusuke.homeip.net/svn/twitter4j/trunk@550 117b7e0d-5933-0410-9d29-ab41bb01d86b

--- twitter4j-examples/bin/asyncUpdate.sh
+++ twitter4j-examples/bin/asyncUpdate.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 . ./setEnv.sh
 
-echo $JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.AsyncUpdate $@
-$JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.AsyncUpdate $@
+echo $JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.AsyncUpdate "$@"
+$JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.AsyncUpdate "$@"
--- twitter4j-examples/bin/feedMonitor.sh
+++ twitter4j-examples/bin/feedMonitor.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 . ./setEnv.sh
 
-echo $JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.FeedMonitor $@
-$JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.FeedMonitor $@
+echo $JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.FeedMonitor "$@"
+$JAVA_HOME/bin/java $MEM_ARGS -cp $CLASSPATH twitter4j.examples.FeedMonitor "$@"
