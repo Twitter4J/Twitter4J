62caf9c05867fe3183c6ffd35764809d21b14878
Yusuke Yamamoto <yusuke@mac.com>
2010-04-06 01:00:57 +0900

    TFJ-213 until is misspelled: untli - fixing test case

--- twitter4j-core/src/test/java/twitter4j/SearchAPITest.java
+++ twitter4j-core/src/test/java/twitter4j/SearchAPITest.java
@@ -47,7 +47,7 @@ public class SearchAPITest extends TwitterTestBase {
     public void testQuery() throws Exception {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         Query query = new Query("test")
-                .until(format.format(new java.util.Date()));
+                .until(format.format(new java.util.Date(System.currentTimeMillis() - 3600*24)));
         HttpParameter[] params = query.asHttpParameterArray();
         assertTrue(findParameter(params,"q"));
         assertTrue(findParameter(params,"until"));
