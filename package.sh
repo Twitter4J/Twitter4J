2e27eca4b3e9f9640027b82c61d989ddc035b4a1
Yusuke Yamamoto <yusuke@mac.com>
2010-04-18 00:29:16 +0900

    renaming twitter4j-apache-httpclient-support artifact to twitter4j-httpclient-support

--- package.sh
+++ package.sh
@@ -4,6 +4,8 @@ cd twitter4j-core
 mvn clean package -Dmaven.test.skip=true
 cd ../twitter4j-examples
 mvn clean package -Dmaven.test.skip=true
+cd ../twitter4j-httpclient-support
+mvn clean package -Dmaven.test.skip=true
 cd ..
 
 DIR=twitter4j-$1
@@ -19,6 +21,11 @@ cp -r twitter4j-examples/src $DIR/twitter4j-examples/
 cp twitter4j-examples/pom.xml $DIR/twitter4j-examples/
 cp -r twitter4j-examples/bin $DIR/
 cp twitter4j-examples/feedmonitor.properties $DIR/twitter4j-examples/
+
+mkdir $DIR/twitter4j-httpclient-support
+cp -r twitter4j-httpclient-support/src $DIR/twitter4j-httpclient-support/
+cp twitter4j-httpclient-support/pom.xml $DIR/twitter4j-httpclient-support/
+
 
 
 cp pom.xml $DIR/pom.xml
@@ -38,8 +45,13 @@ mkdir $DIR/twitter4j-core/javadoc/
 unzip twitter4j-core/target/twitter4j-core-$1-javadoc.jar -d $DIR/twitter4j-core/javadoc/
 
 cp twitter4j-examples/target/twitter4j-examples-$1.jar $DIR/
-mkdir $DIR/twitter4j-examples-javadoc/
+mkdir $DIR/twitter4j-examples/javadoc/
 unzip twitter4j-examples/target/twitter4j-examples-$1-javadoc.jar -d $DIR/twitter4j-examples/javadoc/
+
+cp twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1.jar $DIR/
+mkdir $DIR/twitter4j-httpclient-support/javadoc/
+unzip twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1-javadoc.jar -d $DIR/twitter4j-httpclient-support/javadoc/
+
 
 cd $DIR/
 find . -name ".svn" -print -exec rm -rf {} ";"
