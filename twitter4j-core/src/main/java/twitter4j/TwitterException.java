7f9863d0358ac4eb8efbdf7189c5922c8d85fdc4
Yusuke Yamamoto <yusuke@mac.com>
2010-07-22 16:52:11 +0900

    TFJ-266 mask Basic Authentication header in the debug output: fixing regression

--- twitter4j-core/src/main/java/twitter4j/TwitterException.java
+++ twitter4j-core/src/main/java/twitter4j/TwitterException.java
@@ -163,8 +163,8 @@ public final class TwitterException extends Exception implements TwitterResponse
     /**
      * Returns a hexadecimal representation of this exception stacktrace.<br>
      * An exception code is a hexadecimal representation of the stacktrace which enables it easier to Google known issues.<br>
-     * Format : XX:YY[-XX:YY]<br>
-     * Where XX is a hash code of stacktrace including line number<br>
+     * Format : XXXXXXXX:YYYYYYYY[ XX:YY]<br>
+     * Where XX is a hash code of stacktrace without line number<br>
      * YY is a hash code of stacktrace excluding line number<br>
      * [-XX:YY] will appear when this instance a root cause
      * @return a hexadecimal representation of this exception stacktrace
