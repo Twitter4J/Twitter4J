e286baacaf252f8c4786d5f9598a7cd8267a700d
Yusuke Yamamoto <yusuke@mac.com>
2010-11-20 14:54:48 +0900

    TFJ-422 adding DataObjectFactory examples

--- twitter4j-examples/src/main/java/twitter4j/examples/json/LoadRawJSON.java
+++ twitter4j-examples/src/main/java/twitter4j/examples/json/LoadRawJSON.java
@@ -24,39 +24,76 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-package twitter4j.examples.timeline;
+package twitter4j.examples.json;
 
 import twitter4j.Status;
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.json.DataObjectFactory;
 
-import java.util.List;
+import java.io.*;
 
 /**
- * Example application that gets public, user and friend timeline using specified account.<br>
+ * Example application that load raw JSON forms from statuses/ directory and dump status texts.
+ *
  * @author Yusuke Yamamoto - yusuke at mac.com
  */
-public final class GetPublicTimeline {
+public final class LoadRawJSON {
     /**
-     * Usage: java twitter4j.examples.timeline.GetPublicTimeline
+     * Usage: java twitter4j.examples.json.LoadRawJSON
+     *
      * @param args String[]
      */
     public static void main(String[] args) {
-
-        Twitter unauthenticatedTwitter = new TwitterFactory().getInstance();
-        System.out.println("Showing public timeline.");
         try {
-            List<Status> statuses = unauthenticatedTwitter.getPublicTimeline();
-            for (Status status : statuses) {
-                System.out.println(status.getUser().getName() + ":" +
-                                   status.getText());
+            File[] files = new File("statuses").listFiles(new FilenameFilter(){
+                public boolean accept(File dir, String name){
+                    return name.endsWith(".json");
+                    }
+            });
+            for(File file : files){
+                String rawJSON = readFirstLine(file);
+                Status status = DataObjectFactory.createStatus(rawJSON);
+                System.out.println("@"+ status.getUser().getScreenName() + " - " + status.getText());
             }
             System.exit(0);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println("Failed to store tweets: " + ioe.getMessage());
         } catch (TwitterException te) {
             te.printStackTrace();
             System.out.println("Failed to get timeline: " + te.getMessage());
-            System.exit( -1);
+            System.exit(-1);
+        }
+    }
+
+    private static String readFirstLine(File fileName) throws IOException {
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        try {
+            fis = new FileInputStream(fileName);
+            isr = new InputStreamReader(fis, "UTF-8");
+            br = new BufferedReader(isr);
+            return br.readLine();
+        } finally {
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException ignore) {
+                }
+            }
+            if (null != isr) {
+                try {
+                    isr.close();
+                } catch (IOException ignore) {
+                }
+            }
+            if (null != fis) {
+                try {
+                    fis.close();
+                } catch (IOException ignore) {
+                }
+            }
         }
     }
 }
