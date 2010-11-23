c7c0da84ab5c9619551653ba6aa0ebafb32a104c
Yusuke Yamamoto <yusuke@mac.com>
2010-11-23 23:04:46 +0800

    TFJ-417 createPlace() now returns SimilarPlaces object which contains token TFJ-439 adding Geo Resources examples

---
+++ twitter4j-examples/src/main/java/twitter4j/examples/geo/ReverseGeoCode.java
@@ -0,0 +1,71 @@
+/*
+Copyright (c) 2007-2010, Yusuke Yamamoto
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Yusuke Yamamoto nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY Yusuke Yamamoto ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Yusuke Yamamoto BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package twitter4j.examples.geo;
+
+import twitter4j.GeoLocation;
+import twitter4j.GeoQuery;
+import twitter4j.Place;
+import twitter4j.ResponseList;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+/**
+ * Given a latitude and a longitude, searches for up to 20 places that can be used as a place_id when updating a status.
+ *
+ * @author Yusuke Yamamoto - yusuke at mac.com
+ */
+public final class ReverseGeoCode {
+    /**
+     * Usage: java twitter4j.examples.geo.ReverseGeoCode [latitude] [longitude]
+     *
+     * @param args message
+     */
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java twitter4j.examples.geo.ReverseGeoCode [latitude] [longitude]");
+            System.exit(-1);
+        }
+        try {
+            Twitter twitter = new TwitterFactory().getInstance();
+            GeoQuery query = new GeoQuery(new GeoLocation(Double.parseDouble(args[0]), Double.parseDouble(args[1])));
+            ResponseList<Place> places = twitter.reverseGeoCode(query);
+            if (places.size() == 0) {
+                System.out.println("No location associated with the specified lat/lang");
+            } else {
+                for (Place place : places) {
+                    System.out.println("id: " + place.getId() + " name: " + place.getFullName());
+                }
+            }
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to retrieve places: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+}
