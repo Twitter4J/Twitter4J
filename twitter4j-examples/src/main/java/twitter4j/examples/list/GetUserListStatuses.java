77acf9b7141fdc530402a5dc5b7039701bdd34c7
Yusuke Yamamoto <yusuke@mac.com>
2010-11-24 22:49:30 +0800

    fixing list examples' minor bugs

--- twitter4j-examples/src/main/java/twitter4j/examples/list/GetUserListStatuses.java
+++ twitter4j-examples/src/main/java/twitter4j/examples/list/GetUserListStatuses.java
@@ -45,7 +45,7 @@ public final class GetUserListStatuses {
      * @param args message
      */
     public static void main(String[] args) {
-        if (args.length < 1) {
+        if (args.length < 2) {
             System.out.println("Usage: java twitter4j.examples.list.GetUserListStatuses [list owner screen name] [list id]");
             System.exit(-1);
         }
