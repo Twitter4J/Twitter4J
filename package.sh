830e51907177fb7f538dd7e02b1be3b6a6169ef1
Yusuke Yamamoto <yusuke@mac.com>
2010-12-02 12:12:10 +0900

    removing http-client-support from distribution zip as it is rarely used

--- package.sh
+++ package.sh
@@ -4,8 +4,8 @@ cd twitter4j-core
 mvn clean package -Dmaven.test.skip=true
 cd ../twitter4j-examples
 mvn clean package -Dmaven.test.skip=true
-cd ../twitter4j-httpclient-support
-mvn clean package -Dmaven.test.skip=true
+#cd ../twitter4j-httpclient-support
+#mvn clean package -Dmaven.test.skip=true
 cd ../twitter4j-media-support
 mvn clean package -Dmaven.test.skip=true
 cd ..
@@ -24,9 +24,9 @@ cp twitter4j-examples/pom.xml $DIR/twitter4j-examples/
 cp -r twitter4j-examples/bin $DIR/
 cp twitter4j-examples/feedmonitor.properties $DIR/twitter4j-examples/
 
-mkdir $DIR/twitter4j-httpclient-support
-cp -r twitter4j-httpclient-support/src $DIR/twitter4j-httpclient-support/
-cp twitter4j-httpclient-support/pom.xml $DIR/twitter4j-httpclient-support/
+#mkdir $DIR/twitter4j-httpclient-support
+#cp -r twitter4j-httpclient-support/src $DIR/twitter4j-httpclient-support/
+#cp twitter4j-httpclient-support/pom.xml $DIR/twitter4j-httpclient-support/
 
 mkdir $DIR/twitter4j-media-support
 cp -r twitter4j-media-support/src $DIR/twitter4j-media-support/
@@ -44,28 +44,23 @@ mkdir $DIR/bin/lib
 #cp ~/.m2/repository/rome/rome/0.9/rome-0.9.jar $DIR/bin/lib
 #cp ~/.m2/repository/org/slf4j/slf4j-api/1.5.8/slf4j-api-1.5.8.jar $DIR/bin/lib
 
-cp twitter4j-core/target/twitter4j-core-$1.jar $DIR/
-cp twitter4j-core/target/twitter4j-core-$1-sources.jar $DIR/twitter4j-core/
-cp twitter4j-core/target/twitter4j-core-$1-javadoc.jar $DIR/twitter4j-core/
 mkdir $DIR/twitter4j-core/javadoc/
 unzip twitter4j-core/target/twitter4j-core-$1-javadoc.jar -d $DIR/twitter4j-core/javadoc/
 
-cp twitter4j-media-support/target/twitter4j-media-support-$1.jar $DIR/
-cp twitter4j-media-support/target/twitter4j-media-support-$1-sources.jar $DIR/twitter4j-media-support/
-cp twitter4j-media-support/target/twitter4j-media-support-$1-javadoc.jar $DIR/twitter4j-media-support/
 mkdir $DIR/twitter4j-media-support/javadoc/
 unzip twitter4j-media-support/target/twitter4j-media-support-$1-javadoc.jar -d $DIR/twitter4j-media-support/javadoc/
 
-cp twitter4j-examples/target/twitter4j-examples-$1.jar $DIR/
 mkdir $DIR/twitter4j-examples/javadoc/
 unzip twitter4j-examples/target/twitter4j-examples-$1-javadoc.jar -d $DIR/twitter4j-examples/javadoc/
 
-cp twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1-javadoc.jar $DIR/twitter4j-httpclient-support/
-cp twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1-sources.jar $DIR/twitter4j-httpclient-support/
-cp twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1.jar $DIR/twitter4j-httpclient-support/
+#cp twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1.jar $DIR/twitter4j-httpclient-support/
 #mkdir $DIR/twitter4j-httpclient-support/javadoc/
 #unzip twitter4j-httpclient-support/target/twitter4j-httpclient-support-$1-javadoc.jar -d $DIR/twitter4j-httpclient-support/javadoc/
 
+mkdir $DIR/lib
+cp twitter4j-core/target/twitter4j-core-$1.jar $DIR/lib
+cp twitter4j-media-support/target/twitter4j-media-support-$1.jar $DIR/lib
+cp twitter4j-examples/target/twitter4j-examples-$1.jar $DIR/lib
 
 cd $DIR/
 find . -name ".svn" -print -exec rm -rf {} ";"
