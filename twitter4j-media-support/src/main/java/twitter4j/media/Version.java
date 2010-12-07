4aa74eb45321f46ae65d0be9ed1d6e983427066a
Yusuke Yamamoto <yusuke@mac.com>
2010-12-07 17:50:12 +0900

    start working on 2.1.9

--- twitter4j-media-support/src/main/java/twitter4j/media/Version.java
+++ twitter4j-media-support/src/main/java/twitter4j/media/Version.java
@@ -30,7 +30,7 @@ package twitter4j.media;
  * @author Yusuke Yamamoto - yusuke at mac.com
  */
 public final class Version {
-    private static final String VERSION = "2.1.8";
+    private static final String VERSION = "2.1.9-SNAPSHOT";
     private static final String TITLE = "Twitter4J Media support";
 
     private Version(){
