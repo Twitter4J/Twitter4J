f200dec27403d5f4ebb512a6550499da269a80e3
Yusuke Yamamoto <yusuke@mac.com>
2011-05-05 20:17:12 +0800

    TFJ-584 update list resources examples

--- twitter4j-examples/src/main/java/twitter4j/examples/listsubscribers/CreateUserListSubscription.java
+++ twitter4j-examples/src/main/java/twitter4j/examples/listsubscribers/CreateUserListSubscription.java
@@ -25,20 +25,20 @@ import twitter4j.TwitterFactory;
  *
  * @author Yusuke Yamamoto - yusuke at mac.com
  */
-public final class SubscribeUserList {
+public final class CreateUserListSubscription {
     /**
-     * Usage: java twitter4j.examples.listsubscribers.SubscribeUserList [list owner screen name] [list id]
+     * Usage: java twitter4j.examples.listsubscribers.CreateUserListSubscription [list id]
      *
      * @param args message
      */
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java twitter4j.examples.listsubscribers.SubscribeUserList [list owner screen name] [list id]");
+        if (args.length < 1) {
+            System.out.println("Usage: java twitter4j.examples.listsubscribers.CreateUserListSubscription [list id]");
             System.exit(-1);
         }
         try {
             Twitter twitter = new TwitterFactory().getInstance();
-            twitter.subscribeUserList(args[0], Integer.parseInt(args[1]));
+            twitter.createUserListSubscription(Integer.parseInt(args[0]));
             System.out.println("Successfully subscribed the list.");
             System.exit(0);
         } catch (TwitterException te) {
