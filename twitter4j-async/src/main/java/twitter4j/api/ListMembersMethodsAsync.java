39757e34ccfa4f6cadf8ebc5d9bba157b9793841
Yusuke Yamamoto <yusuke@mac.com>
2011-05-05 17:53:21 +0900

    TFJ-584 update list resouces endpoints

--- twitter4j-async/src/main/java/twitter4j/api/ListMembersMethodsAsync.java
+++ twitter4j-async/src/main/java/twitter4j/api/ListMembersMethodsAsync.java
@@ -22,81 +22,106 @@ package twitter4j.api;
 public interface ListMembersMethodsAsync {
     /**
      * Returns the members of the specified list.
-     * <br>This method calls http://api.twitter.com/1/[user]/[list_id]/members.json
+     * <br>This method calls http://api.twitter.com/1/lists/members.json
      *
      * @param listOwnerScreenName The screen name of the list owner
      * @param listId              The id of the list
      * @param cursor              Breaks the results into pages. A single page contains 20 lists. Provide a value of -1 to begin paging. Provide values as returned to in the response body's next_cursor and previous_cursor attributes to page back and forth in the list.
-     * @see <a href="http://dev.twitter.com/doc/get/:user/:list_id/members">GET :user/:list_id/members | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/get/lists/members">GET lists/members | dev.twitter.com</a>
      * @since Twitter4J 2.1.1
+     * @deprecated use {@link #getUserListMembers(int, long)} instead
      */
     void getUserListMembers(String listOwnerScreenName, int listId, long cursor);
 
     /**
      * Returns the members of the specified list.
-     * <br>This method calls http://api.twitter.com/1/[user]/[list_id]/members.json
+     * <br>This method calls http://api.twitter.com/1/lists/members.json
      *
      * @param listOwnerId The id of the list owner
      * @param listId      The id of the list
      * @param cursor      Breaks the results into pages. A single page contains 20 lists. Provide a value of -1 to begin paging. Provide values as returned to in the response body's next_cursor and previous_cursor attributes to page back and forth in the list.
-     * @see <a href="http://dev.twitter.com/doc/get/:user/:list_id/members">GET :user/:list_id/members | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/get/lists/members">GET lists/members | dev.twitter.com</a>
      * @since Twitter4J 2.1.1
+     * @deprecated use {@link #getUserListMembers(int, long)} instead
      */
     void getUserListMembers(long listOwnerId, int listId, long cursor);
 
     /**
+     * Returns the members of the specified list.
+     * <br>This method calls http://api.twitter.com/1/lists/members.json
+     *
+     * @param listId      The id of the list
+     * @param cursor      Breaks the results into pages. A single page contains 20 lists. Provide a value of -1 to begin paging. Provide values as returned to in the response body's next_cursor and previous_cursor attributes to page back and forth in the list.
+     * @see <a href="http://dev.twitter.com/doc/get/lists/members">GET lists/members | dev.twitter.com</a>
+     * @since Twitter4J 2.2.3
+     */
+    void getUserListMembers(int listId, long cursor);
+
+    /**
      * Adds a member to a list. The authenticated user must own the list to be able to add members to it. Lists are limited to having 500 members.
-     * <br>This method calls http://api.twitter.com/1/[listOwner]/[listId]/members.json
+     * <br>This method calls http://api.twitter.com/1/lists/members/create.json
      *
      * @param listId The id of the list.
      * @param userId The id of the user to add as a member of the list.
-     * @see <a href="http://dev.twitter.com/doc/post/:user/:list_id/members">POST :user/:list_id/members | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/post/lists/members/create">POST lists/members/create | dev.twitter.com</a>
      * @since Twitter4J 2.1.1
      */
     void addUserListMember(int listId, long userId);
 
     /**
      * Adds multiple members to a list, by specifying a comma-separated list of member ids or screen names. The authenticated user must own the list to be able to add members to it. Lists are limited to having 500 members, and you are limited to adding up to 100 members to a list at a time with this method.
-     * <br>This method calls http://api.twitter.com/1/[user]/[list_id]/members/create_all.json
+     * <br>This method calls http://api.twitter.com/1/lists/members/create_all.json
      *
      * @param listId  The id of the list.
      * @param userIds The array of ids of the user to add as member of the list. up to 100 are allowed in a single request.
-     * @see <a href="http://dev.twitter.com/doc/post/:user/:list_id/create_all">POST :user/:list_id/create_all | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/post/lists/members/create_all">POST lists/members/create_all | dev.twitter.com</a>
      * @since Twitter4J 2.1.7
      */
     void addUserListMembers(int listId, long[] userIds);
 
     /**
      * Adds multiple members to a list, by specifying a comma-separated list of member ids or screen names. The authenticated user must own the list to be able to add members to it. Lists are limited to having 500 members, and you are limited to adding up to 100 members to a list at a time with this method.
-     * <br>This method calls http://api.twitter.com/1/[user]/[list_id]/members/create_all.json
+     * <br>This method calls http://api.twitter.com/1/lists/members/create_all.json
      *
      * @param listId      The id of the list.
      * @param screenNames The array of screen names of the user to add as member of the list. up to 100 are allowed in a single request.
-     * @see <a href="http://dev.twitter.com/doc/post/:user/:list_id/create_all">POST :user/:list_id/create_all | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/post/lists/members/create_all">POST lists/members/create_all | dev.twitter.com</a>
      * @since Twitter4J 2.1.7
      */
     void addUserListMembers(int listId, String[] screenNames);
 
     /**
      * Removes the specified member from the list. The authenticated user must be the list's owner to remove members from the list.
-     * <br>This method calls http://api.twitter.com/1/[listOwner]/[listId]/members.json
+     * <br>This method calls http://api.twitter.com/1/lists/members/destroy.json
      *
      * @param listId The id of the list.
      * @param userId The screen name of the member you wish to remove from the list.
-     * @see <a href="http://dev.twitter.com/doc/delete/:user/:list_id/members">DELETE :user/:id/members | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/post/lists/members/destroy">POST lists/members/destroy | dev.twitter.com</a>
      * @since Twitter4J 2.1.0
      */
     void deleteUserListMember(int listId, long userId);
 
     /**
      * Check if a user is a member of the specified list.<br>
-     * <br>This method calls http://api.twitter.com/1/[listOwner]/[listId]/members/id.json
+     * <br>This method calls http://api.twitter.com/1/lists/members/show.json
      *
      * @param listOwnerScreenName The screen name of the list owner
      * @param listId              The id of the list.
      * @param userId              The id of the user who you want to know is a member or not of the specified list.
-     * @see <a href="http://dev.twitter.com/doc/get/:user/:list_id/members/:id">GET :user/:list_id/members/:id | dev.twitter.com</a>
+     * @see <a href="http://dev.twitter.com/doc/get/lists/members/show">GET lists/members/show | dev.twitter.com</a>
      * @since Twitter4J 2.1.0
+     * @deprecated use {@link #showUserListMembership(int, long)} instead
      */
     void checkUserListMembership(String listOwnerScreenName, int listId, long userId);
+
+    /**
+     * Check if a user is a member of the specified list.<br>
+     * <br>This method calls http://api.twitter.com/1/lists/members/show.json
+     *
+     * @param listId The id of the list.
+     * @param userId The id of the user who you want to know is a member or not of the specified list.
+     * @see <a href="http://dev.twitter.com/doc/get/lists/members/show">GET lists/members/show | dev.twitter.com</a>
+     * @since Twitter4J 2.2.3
+     */
+    void showUserListMembership(int listId, long userId);
 }
--- twitter4j-core/src/main/java/twitter4j/TwitterImpl.java
+++ twitter4j-core/src/main/java/twitter4j/TwitterImpl.java
@@ -663,8 +663,7 @@ class TwitterImpl extends TwitterBaseImpl
         if (description != null) {
             httpParams.add(new HttpParameter("description", description));
         }
-        return new UserListJSONImpl(post(conf.getRestBaseURL() + getScreenName() +
-                "/lists.json",
+        return new UserListJSONImpl(post(conf.getRestBaseURL() + "lists/create.json",
                 httpParams.toArray(new HttpParameter[httpParams.size()])), conf);
     }
 
@@ -674,6 +673,7 @@ class TwitterImpl extends TwitterBaseImpl
     public UserList updateUserList(int listId, String newListName, boolean isPublicList, String newDescription) throws TwitterException {
         ensureAuthorizationEnabled();
         List<HttpParameter> httpParams = new ArrayList<HttpParameter>();
+        httpParams.add(new HttpParameter("list_id", listId));
         if (newListName != null) {
             httpParams.add(new HttpParameter("name", newListName));
         }
@@ -681,8 +681,7 @@ class TwitterImpl extends TwitterBaseImpl
         if (newDescription != null) {
             httpParams.add(new HttpParameter("description", newDescription));
         }
-        return new UserListJSONImpl(post(conf.getRestBaseURL() + getScreenName() + "/lists/"
-                + listId + ".json", httpParams.toArray(new HttpParameter[httpParams.size()])), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() + "lists/update.json", httpParams.toArray(new HttpParameter[httpParams.size()])), conf);
     }
 
     /**
@@ -690,17 +689,30 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public PagableResponseList<UserList> getUserLists(String listOwnerScreenName, long cursor) throws TwitterException {
         ensureAuthorizationEnabled();
-        return UserListJSONImpl.createPagableUserListList(get(conf.getRestBaseURL() +
-                listOwnerScreenName + "/lists.json?cursor=" + cursor), conf);
+        return UserListJSONImpl.createPagableUserListList(get(conf.getRestBaseURL() + "lists.json?screen_name=" + listOwnerScreenName + "&cursor=" + cursor), conf);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PagableResponseList<UserList> getUserLists(long listOwnerUserId, long cursor) throws TwitterException {
+        ensureAuthorizationEnabled();
+        return UserListJSONImpl.createPagableUserListList(get(conf.getRestBaseURL() + "lists.json?user_id=" + listOwnerUserId + "&cursor=" + cursor), conf);
     }
 
     /**
      * {@inheritDoc}
      */
     public UserList showUserList(String listOwnerScreenName, int id) throws TwitterException {
-        ensureAuthorizationEnabled();
-        return new UserListJSONImpl(get(conf.getRestBaseURL() + listOwnerScreenName + "/lists/"
-                + id + ".json"), conf);
+        return showUserList(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public UserList showUserList(int listId) throws TwitterException {
+        return new UserListJSONImpl(get(conf.getRestBaseURL() + "lists/show.json?list_id="
+                + listId), conf);
     }
 
     /**
@@ -708,26 +720,31 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public UserList destroyUserList(int listId) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserListJSONImpl(delete(conf.getRestBaseURL() + getScreenName() +
-                "/lists/" + listId + ".json"), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() + "lists/destroy.json?list_id=" + listId), conf);
     }
 
     /**
      * {@inheritDoc}
      */
     public ResponseList<Status> getUserListStatuses(String listOwnerScreenName, int id, Paging paging) throws TwitterException {
-        return StatusJSONImpl.createStatusList(get(conf.getRestBaseURL() + listOwnerScreenName +
-                "/lists/" + id + "/statuses.json", mergeParameters(paging.asPostParameterArray(Paging.SMCP, Paging.PER_PAGE)
-                , INCLUDE_ENTITIES)), conf);
+        return getUserListStatuses(id, paging);
     }
 
     /**
      * {@inheritDoc}
      */
     public ResponseList<Status> getUserListStatuses(long listOwnerId, int id, Paging paging) throws TwitterException {
-        return StatusJSONImpl.createStatusList(get(conf.getRestBaseURL() + listOwnerId +
-                "/lists/" + id + "/statuses.json", mergeParameters(paging.asPostParameterArray(Paging.SMCP, Paging.PER_PAGE)
-                , INCLUDE_ENTITIES)), conf);
+        return getUserListStatuses(id, paging);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ResponseList<Status> getUserListStatuses(int listId, Paging paging) throws TwitterException {
+        return StatusJSONImpl.createStatusList(get(conf.getRestBaseURL() + "lists/statuses.json", mergeParameters(paging.asPostParameterArray(Paging.SMCP, Paging.PER_PAGE)
+                , new HttpParameter[]{new HttpParameter("list_id", listId),
+                INCLUDE_ENTITIES,
+                INCLUDE_RTS})), conf);
     }
 
     /**
@@ -735,17 +752,16 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public PagableResponseList<UserList> getUserListMemberships(String listMemberScreenName, long cursor) throws TwitterException {
         ensureAuthorizationEnabled();
-        return UserListJSONImpl.createPagableUserListList(get(conf.getRestBaseURL() +
-                listMemberScreenName + "/lists/memberships.json?cursor=" + cursor), conf);
+        return UserListJSONImpl.createPagableUserListList(get(conf.getRestBaseURL()
+                + "lists/memberships.json?screen_name=" + listMemberScreenName + "&cursor=" + cursor), conf);
     }
 
     /**
      * {@inheritDoc}
      */
     public PagableResponseList<UserList> getUserListSubscriptions(String listOwnerScreenName, long cursor) throws TwitterException {
-        ensureAuthorizationEnabled();
         return UserListJSONImpl.createPagableUserListList(get(conf.getRestBaseURL() +
-                listOwnerScreenName + "/lists/subscriptions.json?cursor=" + cursor), conf);
+                "lists/subscriptions.json?screen_name=" + listOwnerScreenName + "&cursor=" + cursor), conf);
     }
 
     /**
@@ -793,10 +809,22 @@ class TwitterImpl extends TwitterBaseImpl
     /**
      * {@inheritDoc}
      */
+    public PagableResponseList<User> getUserListMembers(int listId
+            , long cursor) throws TwitterException {
+        ensureAuthorizationEnabled();
+        return UserJSONImpl.createPagableUserList(get(conf.getRestBaseURL() +
+                "lists/members.json?list_id=" + listId + "include_entities="
+                + conf.isIncludeEntitiesEnabled() + "&cursor=" + cursor), conf);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public UserList addUserListMember(int listId, long userId) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserListJSONImpl(post(conf.getRestBaseURL() + getScreenName() +
-                "/" + listId + "/members.json?id=" + userId), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() +
+                "lists/members/create.json?user_id=" + userId + "&list_id=" +
+                listId), conf);
     }
 
     /**
@@ -804,8 +832,11 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public UserList addUserListMembers(int listId, long[] userIds) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserListJSONImpl(post(conf.getRestBaseURL() + getScreenName() +
-                "/" + listId + "/members/create_all.json?user_id=" + T4JInternalStringUtil.join(userIds)), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() +
+                "lists/members/create_all.json",
+                new HttpParameter[]{
+                        new HttpParameter("list_id", listId),
+                        new HttpParameter("user_id", T4JInternalStringUtil.join(userIds))}), conf);
     }
 
     /**
@@ -813,8 +844,11 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public UserList addUserListMembers(int listId, String[] screenNames) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserListJSONImpl(post(conf.getRestBaseURL() + getScreenName() +
-                "/" + listId + "/members/create_all.json?screen_name=" + T4JInternalStringUtil.join(screenNames)), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() +
+                "lists/members/create_all.json",
+                new HttpParameter[]{
+                        new HttpParameter("list_id", listId),
+                        new HttpParameter("screen_name", T4JInternalStringUtil.join(screenNames))}), conf);
     }
 
     /**
@@ -822,18 +856,29 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public UserList deleteUserListMember(int listId, long userId) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserListJSONImpl(delete(conf.getRestBaseURL() + getScreenName() +
-                "/" + listId + "/members.json?id=" + userId), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() +
+                "lists/members/destroy.json",
+                new HttpParameter[]{
+                        new HttpParameter("list_id", listId),
+                        new HttpParameter("user_id", userId)}), conf);
     }
 
     /**
      * {@inheritDoc}
      */
     public User checkUserListMembership(String listOwnerScreenName, int listId, long userId) throws TwitterException {
+        return showUserListMembership(listId, userId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public User showUserListMembership(int listId, long userId) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserJSONImpl(get(conf.getRestBaseURL() + listOwnerScreenName + "/" + listId
-                + "/members/" + userId + ".json?include_entities="
-                + conf.isIncludeEntitiesEnabled()), conf);
+        return new UserJSONImpl(get(conf.getRestBaseURL() +
+                "lists/members/show.json?list_id=" + listId + "&user_id=" +
+                userId + "&include_entities=" +
+                conf.isIncludeEntitiesEnabled()), conf);
     }
 
     /*List Subscribers Methods*/
@@ -843,9 +888,15 @@ class TwitterImpl extends TwitterBaseImpl
      */
     public PagableResponseList<User> getUserListSubscribers(String listOwnerScreenName
             , int listId, long cursor) throws TwitterException {
-        ensureAuthorizationEnabled();
+        return getUserListSubscribers(listId, cursor);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public PagableResponseList<User> getUserListSubscribers(int listId, long cursor) throws TwitterException {
         return UserJSONImpl.createPagableUserList(get(conf.getRestBaseURL() +
-                listOwnerScreenName + "/" + listId + "/subscribers.json?include_entities="
+                "lists/subscribers.json?list_id=" + listId + "include_entities="
                 + conf.isIncludeEntitiesEnabled() + "&cursor=" + cursor), conf);
     }
 
@@ -861,20 +912,47 @@ class TwitterImpl extends TwitterBaseImpl
     /**
      * {@inheritDoc}
      */
-    public UserList unsubscribeUserList(String listOwnerScreenName, int listId) throws TwitterException {
+    public UserList createUserListSubscription(int listId) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserListJSONImpl(delete(conf.getRestBaseURL() + listOwnerScreenName +
-                "/" + listId + "/subscribers.json?id=" + getId()), conf);
+        return new UserListJSONImpl(post(conf.getRestBaseURL() +
+                "lists/subscribers/create.json"
+                , new HttpParameter[]{new HttpParameter("list_id", listId)}), conf);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public UserList unsubscribeUserList(String listOwnerScreenName, int listId) throws TwitterException {
+        return destroyUserListSubscription(listId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public UserList destroyUserListSubscription(int listId) throws TwitterException {
+        ensureAuthorizationEnabled();
+        return new UserListJSONImpl(post(conf.getRestBaseURL() +
+                "lists/subscribers/destroy.json",
+                new HttpParameter[]{new HttpParameter("list_id", listId)})
+                , conf);
     }
 
     /**
      * {@inheritDoc}
      */
     public User checkUserListSubscription(String listOwnerScreenName, int listId, long userId) throws TwitterException {
+        return showUserListSubscription(listId, userId);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public User showUserListSubscription(int listId, long userId) throws TwitterException {
         ensureAuthorizationEnabled();
-        return new UserJSONImpl(get(conf.getRestBaseURL() + listOwnerScreenName + "/" + listId
-                + "/subscribers/" + userId + ".json?include_entities="
-                + conf.isIncludeEntitiesEnabled()), conf);
+        return new UserJSONImpl(get(conf.getRestBaseURL() +
+                "lists/subscribers/show.json?list_id=" + listId +
+                "&user_id=" + userId + "&include_entities=" +
+                conf.isIncludeEntitiesEnabled()), conf);
     }
 
     /*Direct Message Methods */
