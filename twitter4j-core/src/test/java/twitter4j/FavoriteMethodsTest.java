35bb9eb5c0f3b2b4a2e7c212cdd97edeca4b0594
Yusuke Yamamoto <yusuke@mac.com>
2011-07-12 18:29:01 +0900

    trivial test case fix

--- twitter4j-core/src/test/java/twitter4j/FavoriteMethodsTest.java
+++ twitter4j-core/src/test/java/twitter4j/FavoriteMethodsTest.java
@@ -35,7 +35,7 @@ public class FavoriteMethodsTest extends TwitterTestBase {
     }
 
     public void testFavoriteMethods() throws Exception {
-        Status status = twitter1.getHomeTimeline(new Paging().count(1)).get(0);
+        Status status = twitter1.getPublicTimeline().get(0);
         assertNotNull(DataObjectFactory.getRawJSON(status));
         assertEquals(status, DataObjectFactory.createStatus(DataObjectFactory.getRawJSON(status)));
         status = twitter2.createFavorite(status.getId());
