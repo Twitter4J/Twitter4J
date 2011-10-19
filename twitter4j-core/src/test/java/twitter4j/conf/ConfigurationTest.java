5ab2a833f50c61a5322778a38dbfdd33c9b478d0
Yusuke Yamamoto <yusuke@twitter.com>
2011-10-19 17:42:14 +0900

    fix a minor test case error

--- twitter4j-core/src/test/java/twitter4j/conf/ConfigurationTest.java
+++ twitter4j-core/src/test/java/twitter4j/conf/ConfigurationTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2007 Yusuke Yamamoto
+ * Copyright (C) 2007 Yusuke Yamamoto
+ * Copyright (C) 2011 Twitter, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -322,7 +323,7 @@ public class ConfigurationTest extends TestCase {
 
         Configuration t = (Configuration) serializeDeserialize(conf);
 
-        assertSame(conf, (Configuration) serializeDeserialize(conf));
+        assertEquals(conf, (Configuration) serializeDeserialize(conf));
 
         assertTrue(0 == conf.getRestBaseURL().indexOf("http://"));
         assertTrue(0 == conf.getSearchBaseURL().indexOf("http://"));
