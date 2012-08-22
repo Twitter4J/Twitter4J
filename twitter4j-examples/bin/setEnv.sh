11c4c0ef6d1eb81a4f31d454e9e4be68dfb56709
Rajesh Koilpillai <rajesh.koilpillai@gmail.com>
2012-08-22 23:12:41 +0530

    Update twitter4j-examples/bin/setEnv.sh

--- twitter4j-examples/bin/setEnv.sh
+++ twitter4j-examples/bin/setEnv.sh
@@ -3,7 +3,15 @@ if  [ -z $JAVA_HOME ] ; then
  export JAVA_HOME="/Library/Java/Home"
 fi
 
-for jar in ../lib/*.jar;do
+for jar in ../target/*.jar;do
+ export CLASSPATH=$CLASSPATH:$jar
+done
+
+for jar in ../../twitter4j-core/target/*.jar;do
+ export CLASSPATH=$CLASSPATH:$jar
+done
+
+for jar in ../../twitter4j-stream/target/*.jar;do
  export CLASSPATH=$CLASSPATH:$jar
 done
 
