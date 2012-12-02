49f2a8cf7fc2552552f1171233497fb8a5376ff5
Yusuke Yamamoto <yusuke@mac.com>
2012-12-03 00:16:49 +0900

    working on 3.0.3-SNAPSHOT

--- twitter4j-media-support/src/main/java/twitter4j/media/Version.java
+++ twitter4j-media-support/src/main/java/twitter4j/media/Version.java
@@ -20,7 +20,7 @@ package twitter4j.media;
  * @author Yusuke Yamamoto - yusuke at mac.com
  */
 public final class Version {
-    private static final String VERSION = "3.0.2";
+    private static final String VERSION = "3.0.3-SNAPSHOT";
     private static final String TITLE = "Twitter4J Media support";
 
     private Version() {
