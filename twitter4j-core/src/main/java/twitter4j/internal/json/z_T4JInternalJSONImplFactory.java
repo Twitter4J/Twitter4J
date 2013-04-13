54c92865710af943b50e38c89be3ed895fabf4f8
danaja <danajamkdt@gmail.com>
2013-04-13 21:05:08 +0530

    TFJ-766 loading GeoLocation using coordinates field rather than deprecated geo field

--- twitter4j-core/src/main/java/twitter4j/internal/json/z_T4JInternalJSONImplFactory.java
+++ twitter4j-core/src/main/java/twitter4j/internal/json/z_T4JInternalJSONImplFactory.java
@@ -84,8 +84,8 @@ public class z_T4JInternalJSONImplFactory implements z_T4JInternalFactory {
     /*package*/
     static GeoLocation createGeoLocation(JSONObject json) throws TwitterException {
         try {
-            if (!json.isNull("geo")) {
-                String coordinates = json.getJSONObject("geo")
+            if (!json.isNull("coordinates")) {
+                String coordinates = json.getJSONObject("coordinates")
                         .getString("coordinates");
                 coordinates = coordinates.substring(1, coordinates.length() - 1);
                 String[] point = z_T4JInternalStringUtil.split(coordinates, ",");
