c81c44c3339e726d5abb338bc6b84b429997c728
takke <takke30@gmail.com>
2013-12-28 18:23:13 +0900

    added some tests

--- twitter4j-spdy-support/src/test/java/twitter4j/SpdyHttpClientTest.java
+++ twitter4j-spdy-support/src/test/java/twitter4j/SpdyHttpClientTest.java
@@ -16,9 +16,22 @@
 
 package twitter4j;
 
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Properties;
+
 import junit.framework.TestCase;
-import twitter4j.internal.http.HttpClient;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.PropertyConfiguration;
+import twitter4j.internal.http.HttpRequest;
+import twitter4j.internal.http.RequestMethod;
 import twitter4j.internal.http.alternative.HttpClientImpl;
+import twitter4j.internal.org.json.JSONException;
+import twitter4j.internal.org.json.JSONObject;
+
+import com.squareup.okhttp.OkHttpClient;
 
 /**
  * Test case for HttpCient
@@ -27,20 +40,68 @@ import twitter4j.internal.http.alternative.HttpClientImpl;
  * @since Twitter4J 3.x.x
  */
 public class SpdyHttpClientTest extends TestCase {
+    
+    protected Properties p = new Properties();
+    protected Configuration conf;
+    
     public SpdyHttpClientTest(String name) {
         super(name);
     }
 
-    HttpClient client;
-
     protected void setUp() throws Exception {
         super.setUp();
-        client = new HttpClientImpl();
+        
+        InputStream is = SpdyHttpClientTest.class.getResourceAsStream("/test.properties");
+        p.load(is);
+        is.close();
+        
+        conf = new PropertyConfiguration(p);
     }
 
     protected void tearDown() {
     }
     
-    public void testSpdy() {
+    public void testSpdy() throws Exception {
+        HttpClientImpl http = doSomeRequest();
+        
+        // check SPDY
+        Field f = http.getClass().getDeclaredField("client");
+        f.setAccessible(true);
+        OkHttpClient client = (OkHttpClient) f.get(http);
+        assertNotNull(client);  // OkHttpClient was used
+        
+//        ConnectionPool p = client.getConnectionPool();
+//        assertEquals(1, p.getConnectionCount());
+//        assertEquals(0, p.getHttpConnectionCount());
+//        assertEquals(1, p.getSpdyConnectionCount());
+    }
+    
+    public void testNoSpdy() throws Exception {
+        HttpClientImpl.sPreferSpdy = false;
+        
+        HttpClientImpl http = doSomeRequest();
+        
+        // check not SPDY
+        Field f = http.getClass().getDeclaredField("client");
+        f.setAccessible(true);
+        OkHttpClient client = (OkHttpClient) f.get(http);
+        assertNull(client);     // OkHttpClient was NOT used
+    }
+
+    private HttpClientImpl doSomeRequest() throws TwitterException, JSONException {
+        
+        HttpClientImpl http = new HttpClientImpl(conf);
+        String url = "https://api.twitter.com/1.1/account/verify_credentials.json";
+        
+        OAuthAuthorization oauth = new OAuthAuthorization(conf);
+        String accessToken = conf.getOAuthAccessToken();
+        String accessTokenSecret = conf.getOAuthAccessTokenSecret();
+        oauth.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+        
+        HttpRequest req = new HttpRequest(RequestMethod.GET, url, null, oauth, null);
+        JSONObject json = http.request(req).asJSONObject();
+        System.out.println(json.getString("screen_name"));
+        
+        return http;
     }
 }
