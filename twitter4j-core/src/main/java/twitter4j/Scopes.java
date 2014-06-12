79206f89695977c472d755c9fcd0f28e0352f21c
georgeludwig <georgeludwig@gmail.com>
2014-06-12 15:48:44 -0700

    TFJ-852 added Serializable interface to Scope object, created serialization/deserialization test for a Status that has non-null Scope

--- twitter4j-core/src/main/java/twitter4j/Scopes.java
+++ twitter4j-core/src/main/java/twitter4j/Scopes.java
@@ -16,13 +16,15 @@
 
 package twitter4j;
 
+import java.io.Serializable;
+
 /**
  * A data interface representing targeting scopes applied to a status.
  *
  * @author Aaron Rankin - aaron at sproutsocial.com
  * @since Twitter4J 3.0.6
  */
-public interface Scopes {
+public interface Scopes extends Serializable {
 
 	/**
      * Returns the place_ids that identify the scope of the status.
