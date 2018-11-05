318784580bf2f931864af9716f36767346e38425
moko256 <koutaro.mo@gmail.com>
2018-11-05 20:52:23 +0900

    Fix typo
    (cherry picked from commit 3fad91b4f03e69e712aa4042b4b5cf4216eda72c)

--- twitter4j-core/src/main/java/twitter4j/TwitterFactory.java
+++ twitter4j-core/src/main/java/twitter4j/TwitterFactory.java
@@ -35,7 +35,7 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class TwitterFactory implements java.io.Serializable {
     private static final Constructor<Twitter> TWITTER_CONSTRUCTOR;
-    /*AsyncTwitterFactory and TWitterStream will access this field*/
+    /*AsyncTwitterFactory and TwitterStream will access this field*/
     static final Authorization DEFAULT_AUTHORIZATION = AuthorizationFactory.getInstance(ConfigurationContext.getInstance());
     private static final Twitter SINGLETON;
     private static final long serialVersionUID = -563983536986910054L;
