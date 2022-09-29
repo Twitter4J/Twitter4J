e18c778cdffea7fdafea1aed716a853300fba402
Yusuke Yamamoto <yusuke@mac.com>
2022-09-29 22:53:39 +0900

    refactor

--- twitter4j-examples/src/main/java/examples/oauth/GetAccessToken.java
+++ twitter4j-examples/src/main/java/examples/oauth/GetAccessToken.java
@@ -37,11 +37,8 @@ public class GetAccessToken {
     public static void main(String[] args) {
         File file = new File("twitter4j.properties");
         Properties prop = new Properties();
-        InputStream is = null;
-        OutputStream os = null;
-        try {
+        try (InputStream is = new FileInputStream(file)) {
             if (file.exists()) {
-                is = new FileInputStream(file);
                 prop.load(is);
             }
             if (args.length < 2) {
@@ -55,25 +52,13 @@ public class GetAccessToken {
             } else {
                 prop.setProperty("oauth.consumerKey", args[0]);
                 prop.setProperty("oauth.consumerSecret", args[1]);
-                os = new FileOutputStream("twitter4j.properties");
-                prop.store(os, "twitter4j.properties");
+                try (OutputStream os = new FileOutputStream("twitter4j.properties")) {
+                    prop.store(os, "twitter4j.properties");
+                }
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
             System.exit(-1);
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException ignore) {
-                }
-            }
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (IOException ignore) {
-                }
-            }
         }
         try {
             Twitter twitter = Twitter.getInstance();
@@ -114,22 +99,13 @@ public class GetAccessToken {
             System.out.println("Access token: " + accessToken.getToken());
             System.out.println("Access token secret: " + accessToken.getTokenSecret());
 
-            try {
+            try (OutputStream os = new FileOutputStream(file)) {
                 prop.setProperty("oauth.accessToken", accessToken.getToken());
                 prop.setProperty("oauth.accessTokenSecret", accessToken.getTokenSecret());
-                os = new FileOutputStream(file);
                 prop.store(os, "twitter4j.properties");
-                os.close();
             } catch (IOException ioe) {
                 ioe.printStackTrace();
                 System.exit(-1);
-            } finally {
-                if (os != null) {
-                    try {
-                        os.close();
-                    } catch (IOException ignore) {
-                    }
-                }
             }
             System.out.println("Successfully stored access token to " + file.getAbsolutePath() + ".");
             System.exit(0);
