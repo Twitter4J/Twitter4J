78e55accf4d20260dc0644e12e98b536a8215b93
Yusuke Yamamoto <yusuke@mac.com>
2022-11-04 12:00:59 +0900

    fix outdated test case

--- twitter4j-core/src/test-jsonschema/java/twitter4j/JSONSchemaNoPropertiesTest.java
+++ twitter4j-core/src/test-jsonschema/java/twitter4j/JSONSchemaNoPropertiesTest.java
@@ -1,27 +1,43 @@
 package twitter4j;
 
-import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Map;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class JSONSchemaNoPropertiesTest {
     @Test
-    void noProp() throws IOException, URISyntaxException {
-        // #/components/schemas/InvalidRequestProblem/properties/errors/items/properties/parameters
-        Path file = Path.of(Objects.requireNonNull(SchemaLoader.class.getResource("/openapi.json")).toURI());
-
-        @Language("JSON") String apiJsonLatest = new String(Files.readAllBytes(file));
-
-        Map<String, JSONSchema> extract = JSONSchema.extract("#/components/schemas/", apiJsonLatest);
-        JSONSchema jsonSchema = extract.get("#/components/schemas/InvalidRequestProblem/properties/errors/items/properties/parameters");
+    void noProp() {
+        Map<String, JSONSchema> extract = JSONSchema.extract("#/components/schemas/", """
+                {
+                  "components": {
+                    "schemas": {
+                      "errors": {
+                        "type": "array",
+                        "items": {
+                          "type": "object",
+                          "properties": {
+                            "parameters": {
+                              "type": "object",
+                              "additionalProperties": {
+                                "type": "array",
+                                "items": {
+                                  "type": "string"
+                                }
+                              }
+                            },
+                            "message": {
+                              "type": "string"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+                """);
+        JSONSchema jsonSchema = extract.get("#/components/schemas/errors/items/properties/parameters");
         assertFalse(jsonSchema.hasAnyProperties());
 
     }
